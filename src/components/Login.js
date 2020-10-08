7d4ee4a876eab2a65b122e2288f65b4dc6335362
Corey Calhoun <corey.calhoun@dkgconceptsllc.com>
2020-10-08 17:35:56 -0500

    --- Auto-Git Commit ---

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Login.css'
+
+function Login() {
+    return (
+        <div className="login">
+            {/* Spotify Logo */}
+            {/* Login with Spotify Button */}
+            <h2>Login Page Component</h2>
+        </div>
+    )
+}
+
+export default Login
