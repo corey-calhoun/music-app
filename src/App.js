da719538ff4422535d38233652ada9ca73975530
Corey Calhoun <corey.calhoun@dkgconceptsllc.com>
2020-10-08 18:06:05 -0500

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Login from './components/Login'
 function App() {
   return (
     <div className="App">
-      <h1>Music App</h1>
       <Login />
     </div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -4,9 +4,12 @@ import './Login.css'
 function Login() {
     return (
         <div className="login">
-            {/* Spotify Logo */}
-            {/* Login with Spotify Button */}
-            <h2>Login Page Component</h2>
+            <img 
+             className="app__logo"
+             src="https://www.nicepng.com/png/full/48-482191_download-now-from-spotify-wind-turbine-icon.png" 
+             alt="" />
+            <h1>Welcome to Your Music Hub</h1>
+            <a>Login with Spotify</a>
         </div>
     )
 }
