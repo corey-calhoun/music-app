a17fe6446a8ae349c0c39d4f1f352db7d281b63c
Corey Calhoun <corey.calhoun@dkgconceptsllc.com>
2020-10-08 19:36:35 -0500

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './assets/css/App.css';
 import Login from './components/Login'
+import { getTokenFromUrl } from './spotify'
+import SpotifyWebApi from 'spotify-web-api-js'
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+
+  const [token, setToken] = useState(null);
+
+
+  // run code based on condition when app loads
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if(_token) {
+      setToken(_token)
+      spotify.setAccessToken(_token); // giving asccess token to Api
+      spotify.getMe().then(user => {
+        console.log(user);
+      });
+    }
+  }, []);
+
   return (
     <div className="App">
-      <Login />
+      {
+        token ? (
+          <h1>You're logged in</h1>
+        ) : (
+          <Login />
+        )
+      }
+     
     </div>
   );
 }
