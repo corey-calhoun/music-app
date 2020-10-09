c939689e0deaae8cbf8f19006094a6b1f1ba86d1
Corey Calhoun <corey.calhoun@dkgconceptsllc.com>
2020-10-08 22:07:25 -0500

    --- Auto-Git Commit ---

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import './Sidebar.css'
+import SidebarOption from './SidebarOption'
+import HomeIcon from '@material-ui/icons/Home'
+import Search from '@material-ui/icons/Search'
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
 
 function Sidebar() {
     return (
@@ -8,7 +12,17 @@ function Sidebar() {
              className="sidebar__logo"
              src="https://www.nicepng.com/png/full/48-482191_download-now-from-spotify-wind-turbine-icon.png" 
              alt="" />
-            
+            <SidebarOption Icon={HomeIcon} title="Home" />
+            <SidebarOption Icon={Search} title="Search" />
+            <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
+            <br />
+            <strong className="sidebar__title">Playlists</strong>
+            <hr />
+
+            <SidebarOption title="Hip Hop" />
+            <SidebarOption title="Rock" />
+            <SidebarOption title="Aerosmith Radio" />
+            <SidebarOption title="Chill" />
         </div>
     )
 }
