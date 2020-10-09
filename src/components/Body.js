4c77158f815a9692db0c1bfdeb7fe42722fec1c7
Corey Calhoun <corey.calhoun@dkgconceptsllc.com>
2020-10-09 03:39:19 -0500

    --- Auto-Git Commit ---

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,8 @@ import React from 'react'
 import './Body.css'
 import Header from '../components/Header'
 import { useDataLayerValue } from './DataLayer'
+import { Favorite, MoreHoriz, PlayCircleFilled } from '@material-ui/icons';
+import SongRow from '../components/SongRow'
 
 function Body({spotify}) {
 
@@ -17,6 +19,18 @@ function Body({spotify}) {
                     <strong>PLAYLIST</strong>
                     <h2>New Music Friday</h2>
                     <p>{discover_weekly?.description}</p> 
+                </div>
+
+                <div className="body__songs">
+                    <div className="body__icons">
+                      <PlayCircleFilled className="body__shuffle" />
+                      <Favorite fontSize="large"/>
+                      <MoreHoriz />
+                    </div>
+
+                    {discover_weekly?.tracks.items.map((item) => {
+                        <SongRow track={item.track} />
+                    })}
                 </div>
             </div>
         </div>
